82b393fd90bca71d6dfc99238a9f99a7aa6644e9
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 13:50:47 +0900

    mainメソッドから素数計算の繰り返し構文を切り出し

--- src/main/java/Maventest/Maventest/App.java
+++ src/main/java/Maventest/Maventest/App.java
@@ -1,31 +1,16 @@
 package Maventest.Maventest;
-/**
- * Hello world!
- *
- */
+import java.util.*;
+
 public class App
 {
     public static void main( String[] args )
     {
-    	for (int testNumber = 2; testNumber <= 10; testNumber++){
-    		/*素数(その数でしか割りきれない数字)かどうかを判定する*/
-        	/*メソッドへの切り出し方
-        	 * 1.このブロックで使われている変数を全て抜き出す
-        	 * 2.変数の宣言箇所をみる。
-        	 * 宣言がコンテキストの外側宣言→引数として渡す
-        	 * 
-        	 * */
-    		//機能的なメソッドの中から数学的なメソッドを別クラスのメソッドとして切り出す
-    		//切り出したclassからのアクセスにする
-    		if(PrimeNumberCalculator.isPrimeNumber(testNumber)){
-        		System.out.println(testNumber);
-        	}
-    		//System.out.println("sayHello");
-    		sayHello();
-    	}
+    	printNumbers(PrimeNumberCalculator.calcTo(100));
     }
     //メソッドとして切り出し
-    private static void sayHello(){
-		System.out.println("sayHello");
+    private static void printNumbers(List<Integer> numbers){
+    	for (int number: numbers){
+    		System.out.println(number);
+    	}
 	}
 }
--- src/main/java/Maventest/Maventest/PrimeNumberCalculator.java
+++ src/main/java/Maventest/Maventest/PrimeNumberCalculator.java
@@ -1,6 +1,17 @@
 package Maventest.Maventest;
+import java.util.*;
 
 public class PrimeNumberCalculator {
+	public static List<Integer> calcTo(int maxNumber){
+		List<Integer> primeNumbers = new ArrayList();
+		for (int testNumber = 2; testNumber <= maxNumber; testNumber++){
+    		if(isPrimeNumber(testNumber)){
+        		primeNumbers.add(testNumber);
+        	}
+    	}
+		return primeNumbers;
+	
+	}
 	//Appから呼べるようにpublicに変更
     public static boolean isPrimeNumber(int testNumber){
    		for (int i = 2; i < testNumber; i++){
