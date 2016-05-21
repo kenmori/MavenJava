ebe4bef5a675cc6aabb94acf74643c3a9de6b713
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 11:09:12 +0900

    isPrimeNumberの切り出し

--- src/main/java/Maventest/Maventest/App.java
+++ src/main/java/Maventest/Maventest/App.java
@@ -11,16 +11,25 @@ public class App
     {
     	for (int testNumber = 2; testNumber <= 10; testNumber++){
     		/*素数(その数でしか割りきれない数字)かどうかを判定する*/
-    		boolean isPrimeNumber = true;
-    		for (int i = 2; i < testNumber; i++){
-    			if(testNumber % i == 0){
-    				isPrimeNumber = false;
-    				break;
-    			}
-    		}
-    		if (isPrimeNumber) {
-    			System.out.println(testNumber);
-    		}
+        	/*メソッドへの切り出し方
+        	 * 変数の宣言箇所をみる。
+        	 * 宣言がコンテキストの外側宣言→引数として渡す
+        	 * 
+        	 * */
+    		if(isPrimeNumber(testNumber)){
+        		System.out.println(testNumber);
+        	}
     	}
+
+    }
+    private static boolean isPrimeNumber(int testNumber){
+    	boolean isPrimeNumber = true;
+   		for (int i = 2; i < testNumber; i++){
+			if(testNumber % i == 0){
+				isPrimeNumber = false;
+				break;
+			}
+		}
+    	return isPrimeNumber;
     }
 }
