43dd314e4fe9489c056f886f2ef577dbacd862f4
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 15:18:07 +0900

    素数リファクタリング

--- src/main/java/Maventest/Maventest/PrimeNumberCalculator.java
+++ src/main/java/Maventest/Maventest/PrimeNumberCalculator.java
@@ -3,18 +3,30 @@ import java.util.*;
 
 public class PrimeNumberCalculator {
 	public static List<Integer> calcTo(int maxNumber){
+		if(maxNumber < 2){
+			return Collections.emptyList();
+		}
 		List<Integer> primeNumbers = new ArrayList();
-		for (int testNumber = 2; testNumber <= maxNumber; testNumber++){
+		primeNumbers.add(2);
+		for (int testNumber = 3; testNumber <= maxNumber; testNumber = testNumber + 2){
     		if(isPrimeNumber(testNumber)){
         		primeNumbers.add(testNumber);
         	}
     	}
 		return primeNumbers;
-	
+
 	}
 	//Appから呼べるようにpublicに変更
     public static boolean isPrimeNumber(int testNumber){
-   		for (int i = 2; i < testNumber; i++){
+    	if(testNumber < 2){
+    		return false;
+    	} else if (testNumber == 2) {
+    		return true;
+    	} else if (testNumber % 2 == 0) {
+    		return false;
+    	}
+
+   		for (int i = 3; i < testNumber; i = i + 2){
 			if(testNumber % i == 0){
 				return false;
 			}
