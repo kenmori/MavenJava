480b8aa5d42fff10cdf58354eba4b7d5790b8e18
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 11:13:11 +0900

    unuse valuable

--- src/main/java/Maventest/Maventest/App.java
+++ src/main/java/Maventest/Maventest/App.java
@@ -12,7 +12,8 @@ public class App
     	for (int testNumber = 2; testNumber <= 10; testNumber++){
     		/*素数(その数でしか割りきれない数字)かどうかを判定する*/
         	/*メソッドへの切り出し方
-        	 * 変数の宣言箇所をみる。
+        	 * 1.このブロックで使われている変数を全て抜き出す
+        	 * 2.変数の宣言箇所をみる。
         	 * 宣言がコンテキストの外側宣言→引数として渡す
         	 * 
         	 * */
@@ -23,13 +24,11 @@ public class App
 
     }
     private static boolean isPrimeNumber(int testNumber){
-    	boolean isPrimeNumber = true;
    		for (int i = 2; i < testNumber; i++){
 			if(testNumber % i == 0){
-				isPrimeNumber = false;
-				break;
+				return false;
 			}
 		}
-    	return isPrimeNumber;
+    	return true;
     }
 }
