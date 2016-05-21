9ba43482733754145750b21f70c281631925e9be
kenji <kennjimoritata@yahoo.co.jp>
2016-05-21 17:20:51 +0900

    fix Message.java

---
+++ src/main/java/Maventest/Maventest/Message.java
@@ -0,0 +1,7 @@
+package Maventest.Maventest;
+
+public class Message {
+	public static final String HOLIDAYS = "今日は休日です。しっかりと羽をのばしましょう";
+	public static final String FRIDAY = "やっと金曜日です。もうちょっとだけ頑張りましょう";
+	public static final String WEEKDAYS = "今日は平日です。お仕事頑張ってください";
+}
